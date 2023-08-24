09ce38f0b03ea4bc6cbe8b0b286868b8b7ec41bf
MohamedSalesforce <139841560+MohamedSalesforce@users.noreply.github.com>
2023-08-24 16:06:34 +0530

    WireMethod

--- force-app/main/default/lwc/displayCalculator/displayCalculator.js
+++ force-app/main/default/lwc/displayCalculator/displayCalculator.js
@@ -1,56 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class DisplayCalculator extends LightningElement {
+export default class Calculator extends LightningElement {
+    displayValue = '';
 
-    @track displayText = '';
-    @track numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    @track currentNumber = '';
-    @track currentOperation = '';
-    @track previousNumber = '';
-
-    handleNumberClick(event) {
-        this.currentNumber += event.target.dataset.number;
-        this.displayText = this.currentNumber;
+    handleButtonClick(event) {
+        this.displayValue += event.target.value;
     }
 
-    handleOperationClick(event) {
-        this.previousNumber = this.currentNumber;
-        this.currentNumber = '';
-        this.currentOperation = event.target.dataset.operation;
-    }
-
-    handleClearClick() {
-        this.currentNumber = '';
-        this.previousNumber = '';
-        this.currentOperation = '';
-        this.displayText = '';
-    }
-
-    handleOperationClick() {
-        const num1 = parseFloat(this.previousNumber);
-        const num2 = parseFloat(this.currentNumber);
-        let result;
-
-        switch (this.currentOperation) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                result = num1 / num2;
-                break;
-            default:
-                break;
+    calculateResult() {
+        try {
+            this.displayValue = eval(this.displayValue);
+        } catch (error) {
+            this.displayValue = 'Error';
         }
+    }
 
-        this.displayText = result.toString();
-        this.currentNumber = result.toString();
-        this.previousNumber = '';
-        this.currentOperation = '';
+    clearDisplay() {
+        this.displayValue = '';
     }
 }
